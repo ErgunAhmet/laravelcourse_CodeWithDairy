dd0872d8878bb34e8e77a6c1800bd955a53bba25
ahmet <80220613+ErgunAhmet@users.noreply.github.com>
2023-01-21 23:41:37 +0100

    first route and controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +13,27 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+//laravel 8
+Route::get('/products', [ProductsController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
+
+/*
+//Route to user
+Route::get('/users', function() {
+    return 'Welcome to the users page';
 });
+
+//Route to users
+Route::get('/users', function() {
+    return ['php', 'laravel'];
+});
+
+//Route to users
+Route::get('/usersJson', function() {
+    return response()->json(['name' => 'Ahmet', 'laravel' => 'course']);
+});
+
+//Route to users -function
+Route::get('/usersFunction', function() {
+    return redirect('/');
+});*/
